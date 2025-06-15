30456e979631f4d351a067e0da31f5dc87598989
Baibhav Tiwari <97496657+lexendx@users.noreply.github.com>
2025-06-15 17:00:06 +0530

    const variable

--- variables/variables.js
+++ variables/variables.js
@@ -16,6 +16,15 @@
     year=87;
     // teeno value update ho rhi hai.
 
-    
+// const = variable can not be re-declear or updated. a block scope variable.
+// iski value ko change nhi kiya ja sakta hai.
+const umar = 24;
+umar= 59;
+umar= 86;
+console.log(umar);
+//this is not possible in js
+// Example
+const PI = 3.14;
+console.log(PI);
     
 
