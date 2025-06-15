2b132ff7680e7687eb67a22a9a0801e76f39496d
Baibhav Tiwari <97496657+lexendx@users.noreply.github.com>
2025-06-15 16:32:59 +0530

    var

---
+++ variables/variables.js
@@ -0,0 +1,9 @@
+//let, var const 
+// var= variable can be redeclear & updated a global scope variable
+ let fullnamename = "baibhav tiwari"
+  let age = 24;
+  totalPrice=1000;
+  console.log(fullname);
+   console.log(age);
+    console.log(totalPrice);
+
